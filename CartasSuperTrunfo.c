b9819e8ca584eefa16247dafc4937fa1688eb630
Dênio <deniomaxx@gmail.com>
2026-03-12 02:38:24 +0000

    Declarando as variáveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,21 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
 
-  // Área para entrada de dados
+int main(){
+    //Declarando variáveis
+    char estado[20], capital[20], codigoCarta[3];
+    int populacao, pontosTuristicos;
+    float area, pib;
 
-  // Área para exibição dos dados da cidade
+    /*
+          estado[20]: nome do estado
+         capital[20]: nome da capital
+      codigoCarta[3]: código de dois dígitos de identificação da carta
+           populacao: quantidade de habitantes do estado
+    pontosTuristicos: quantidade de pontos turísticos do estado
+                area: tamanho territorial, em km², doo estado
+                 pib: renda per capita do estado
+    */
 
-return 0;
-} 
+}
